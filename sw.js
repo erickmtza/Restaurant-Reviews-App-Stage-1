bcbf930eb13973e6b72da861ee4f00f03674c087
Erick Martinez <777areri@gmail.com>
2018-09-30 20:23:10 -0500

    Able to utilize the cache by fetching for it

--- sw.js
+++ sw.js
@@ -26,3 +26,14 @@ self.addEventListener('install', function(event) {
         })
     );
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+          if(response) {
+              return response;
+          }
+          return fetch(event.request);
+        })
+    )
+})
